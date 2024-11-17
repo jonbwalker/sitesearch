86037651f1b3dad4ae5428d3a0894cb4baf28815
jonbwalker <jonathan.b.walker0@gmail.com>
2024-11-17 10:48:22 -0800

    convert config to boolean

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     searchUrl: process.env.SEARCH_URL,
     resultClass: process.env.RESULT_CLASS,
     twilioConfig: {
-        enabled: process.env.TWILIO_ENABLED,
+        enabled: process.env.TWILIO_ENABLED === 'true',
         accountSid: process.env.TWILIO_ACCOUNT_SID,
         authToken: process.env.TWILIO_AUTH_TOKEN,
         toPhoneNumber: process.env.TWILIO_TO_PHONE_NUMBER,
